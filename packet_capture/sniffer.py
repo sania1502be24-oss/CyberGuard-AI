238429f9dd06c808f89937b17a9704321a8e7b02
sania1502be24-oss <sania1502.be24@chitkarauniversity.edu.in>
2026-07-22 10:27:52 +0530

    Day 2: Added packet capture using Scapy

---
+++ packet_capture/sniffer.py
@@ -0,0 +1,8 @@
+from scapy.all import sniff
+
+def packet_callback(packet):
+    print(packet.summary())
+
+print("Starting packet capture...")
+sniff(prn=packet_callback, count=10)
+print("Capture complete!")
